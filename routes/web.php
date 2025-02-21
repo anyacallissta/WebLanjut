6ae3d034bbd933c5ef1ee89c04ebdcb1270ffc33
anyacallissta <vtjhs18@gmail.com>
2025-02-21 15:12:47 +0700

    add some route from jobsheet 2

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,41 @@ Route::get('/', function () { // Rute untuk halaman utama ('/'), mengembalikan t
 });
 
 Route::resource('items', ItemController::class); // Menyediakan resource route dan secara otomatis membuat rute CRUD
+
+// jobsheet 2
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return 'NIM: 2341720234 | Nama: Anya Callissta Chriswantari';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama Saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' .$id;
+});
+
+// Route::get('/user/{name?}', function ($name=null) {
+//     return 'Nama Saya '.$name;
+// });
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+});
+
