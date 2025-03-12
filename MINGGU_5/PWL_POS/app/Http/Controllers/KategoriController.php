eaea4d6152cf45cef25a0af93f19cc4aa04fc0e8
anyacallissta <vtjhs18@gmail.com>
2025-03-12 14:34:33 +0700

    JS5 - praktikum 1, 2, dan 3

---
+++ MINGGU_5/PWL_POS/app/Http/Controllers/KategoriController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\KategoriModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\DataTables\KategoriDataTable;
+
+class KategoriController extends Controller
+{
+    // public function index()
+    // {
+    //     // langkah 3
+    //     // $data = [
+    //     //     'kategori_kode' => 'SNK',
+    //     //     'kategori_nama' => 'Snack/Makanan Ringan',
+    //     //     'created_at' => now()
+    //     // ];
+
+    //     // DB::table('m_kategori')->insert($data);
+    //     // return 'Insert data baru berhasil';
+
+    //     // langkah 5
+    //     // $row = DB::table('m_kategori')
+    //     // ->where('kategori_kode', 'SNK')
+    //     // ->update(['kategori_nama' => 'Camilan']);
+
+    //     // return 'Update data berhasil. Jumlah data yang diupdate: ' . $row . ' baris';
+
+    //     // langkah 7
+    //     // $row = DB::table('m_kategori')
+    //     // ->where('kategori_kode', 'SNK')
+    //     // ->delete();
+
+    //     // return 'Delete data berhasil. Jumlah data yang dihapus: ' . $row . ' baris';
+
+    //     // langkah 8
+    //     $data = DB::table('m_kategori')->get();
+    //     return view('kategori', ['data' => $data]);
+    // }
+
+    public function index(KategoriDataTable $dataTable) {
+        return $dataTable->render('kategori.index');
+    }
+
+    public function create() {
+        return view('kategori.create');
+    }
+
+    public function store(Request $request) {
+        KategoriModel::create([
+            'kategori_kode' => $request->kodeKategori,
+            'kategori_nama' => $request->namaKategori,
+        ]);
+        return redirect('/kategori');
+    }
+}
