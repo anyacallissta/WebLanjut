de8c79a6a299e415141d087bed92de47d44b30a4
anyacallissta <vtjhs18@gmail.com>
2025-03-22 23:05:08 +0700

    JS6 - praktikum 2

--- MINGGU_6/PWL_POS/routes/web.php
+++ MINGGU_6/PWL_POS/routes/web.php
@@ -49,6 +49,11 @@ Route::group(['prefix' => 'user'], function () {
     Route::get('/{id}', [UserController::class, 'show']); // menampilkan detail user
     Route::get('/{id}/edit', [UserController::class, 'edit']); // menampilkan halaman form edit user
     Route::put('/{id}', [UserController::class, 'update']); // menyimpan perubahan data user
+
+    // jobsheet 6
+    Route::get('/{id}/edit_ajax', [UserController::class, 'edit']); // menampilkan halaman form edit user ajax
+    Route::put('/{id}/update_ajax', [UserController::class, 'update']); // menyimpan perubahan data user ajax
+
     Route::delete('/{id}', [UserController::class, 'destroy']); // menghapus data user
 });
 
