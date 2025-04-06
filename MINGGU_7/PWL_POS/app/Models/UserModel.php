99681bbeb6a19e66759991f36bb35b5a6ce85029
anyacallissta <vtjhs18@gmail.com>
2025-04-07 04:47:40 +0700

    JS7 - praktikum 1

--- MINGGU_7/PWL_POS/app/Models/UserModel.php
+++ MINGGU_7/PWL_POS/app/Models/UserModel.php
@@ -5,9 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User as Authenticatable; //Implementasi class Authenticatable
 
-
-class UserModel extends Model
+class UserModel extends Authenticatable
 {
     use HasFactory;
 
@@ -18,11 +18,17 @@ class UserModel extends Model
         'level_id',
         'username',
         'nama',
-        'password'
+        'password',
+        'created_at',
+        'updated_at'
     ];
 
+    protected $hidden = ['password'];
+
+    protected $casts = ['password' => 'hashed'];
+
     public function level():BelongsTo{
-        return $this->belongsTo(LevelModel::class, 'level_id','level_id');
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
 
 }
